868552eb0f8fdf1f55f56e1aec7580ba8d8aa979
Chris <chris.hubert@buildasign.com>
2021-04-30 09:08:56 -0500

    init

---
+++ GigHub2/Models/Gig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GigHub2.Models
+{
+    public class Gig
+    {
+        public int ID { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
